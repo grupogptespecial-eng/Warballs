aed0665f43388ccdc0ee219ee8d715a61ceda640
grupogptespecial-eng <grupogpt.especial@gmail.com>
2025-08-16 21:32:59 -0300

    Add wolf spirit SVG asset

--- src/entities/summon.js
+++ src/entities/summon.js
@@ -3,6 +3,9 @@
 import { V } from '../math/vec.js';
 import { randAng } from '../utils/rand.js';
 
+const spiritFaceImg = (typeof Image !== 'undefined') ? new Image() : { complete: false };
+if (spiritFaceImg.src !== undefined) spiritFaceImg.src = 'assets/ranger_spirit.svg';
+
 export class Summon {
   constructor(owner, pos, dmg, duration) {
     this.owner = owner;
@@ -46,7 +49,7 @@ export class Summon {
 
   draw(ctx) {
     ctx.save();
-    ctx.globalAlpha = .9;
+    ctx.globalAlpha = 0.9;
     ctx.fillStyle = 'rgba(124,255,176,0.85)';
     ctx.shadowColor = '#7cffb0';
     ctx.shadowBlur = 12;
@@ -54,6 +57,20 @@ export class Summon {
     ctx.arc(this.pos.x, this.pos.y, this.bodyR * 0.8, 0, Math.PI * 2);
     ctx.fill();
     ctx.restore();
+
+    if (spiritFaceImg.complete) {
+      const size = this.bodyR * 2.2;
+      const x = this.pos.x - size * 0.5;
+      const y = this.pos.y - size * 0.5;
+      ctx.save();
+      ctx.globalAlpha = 0.4;
+      ctx.drawImage(spiritFaceImg, x, y, size, size);
+      ctx.globalCompositeOperation = 'source-atop';
+      ctx.fillStyle = 'rgba(124,255,176,0.6)';
+      ctx.globalAlpha = 0.6;
+      ctx.fillRect(x, y, size, size);
+      ctx.restore();
+    }
   }
 }
 
