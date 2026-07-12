c27e819814b94e4fd7f5698ab537af86371ef844
grupogptespecial-eng <grupogpt.especial@gmail.com>
2026-07-12 16:49:07 -0300

    Add compile-only annotation required by R8

--- lg-wifi-remote-app/app/build.gradle.kts
+++ lg-wifi-remote-app/app/build.gradle.kts
@@ -119,6 +119,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("androidx.security:security-crypto:1.0.0")
+    compileOnly("com.google.errorprone:error_prone_annotations:2.36.0")
 
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
